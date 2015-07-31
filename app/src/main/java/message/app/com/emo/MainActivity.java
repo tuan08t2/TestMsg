80b65b9fe534c14f8be0efd514ad7e74630a28cc
tuannv <tuannv@asiantech.vn>
2015-07-31 14:44:39 +0700

    Drag icon

--- app/src/main/java/message/app/com/emo/MainActivity.java
+++ app/src/main/java/message/app/com/emo/MainActivity.java
@@ -1,8 +1,13 @@
 package message.app.com.emo;
 
+import android.content.ClipDescription;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -19,7 +24,7 @@ import message.app.com.emo.adapter.PagerAdapter;
 import message.app.com.emo.fragments.EmojiconFragment_;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnDragListener{
 
     @ViewById(R.id.tabs)
     protected PagerSlidingTabStrip mTab;
@@ -40,11 +45,19 @@ public class MainActivity extends ActionBarActivity {
     protected LinearLayout mLlChatContent;
 
     private PagerAdapter mAdapter;
+    private String TAG = "xxx";
 
     @AfterViews
     public void init() {
+        initDrag();
         initPager();
         initTab();
+    }
+
+    public void initDrag() {
+        mImgFirst.setOnDragListener(this);
+        mImgSecond.setOnDragListener(this);
+        mVp.setOnDragListener(this);
     }
 
     public void initPager() {
@@ -66,4 +79,73 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    @Override
+    public boolean onDrag(View imageViewTarget, DragEvent dragEvent) {
+
+        View draggedImageView = (View) dragEvent.getLocalState();
+
+        // Handles each of the expected events
+        switch (dragEvent.getAction()) {
+
+            case DragEvent.ACTION_DRAG_STARTED:
+                Log.i(TAG, "drag action started");
+
+                // Determines if this View can accept the dragged data
+                if (dragEvent.getClipDescription()
+                        .hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    Log.i(TAG, "Can accept this data");
+
+                    // returns true to indicate that the View can accept the dragged data.
+                    return true;
+
+                } else {
+                    Log.i(TAG, "Can not accept this data");
+
+                }
+
+                // Returns false. During the current drag and drop operation, this View will
+                // not receive events again until ACTION_DRAG_ENDED is sent.
+                return false;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                Log.i(TAG, "drag action entered");
+//                the drag point has entered the bounding box
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                Log.i(TAG, "drag action location");
+                /*triggered after ACTION_DRAG_ENTERED
+                stops after ACTION_DRAG_EXITED*/
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                Log.i(TAG, "drag action exited");
+//                the drag shadow has left the bounding box
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                  /* the listener receives this action type when
+                  drag shadow released over the target view
+                 the action only sent here if ACTION_DRAG_STARTED returned true
+                 return true if successfully handled the drop else false*/
+                Log.i("size", dragEvent.getClipData().getItemCount() + "");
+                int resourceId = Integer.parseInt(dragEvent.getClipData().getItemAt(0).getText().toString());
+                Log.i(TAG, "Tennis ball");
+                ViewGroup draggedImageViewParentLayout
+                        = (ViewGroup) draggedImageView.getParent();
+                draggedImageViewParentLayout.removeView(draggedImageView);
+                if(imageViewTarget instanceof  ImageView) {
+                    ImageView imgTarget = (ImageView) imageViewTarget;
+                    imgTarget.setImageResource(resourceId);
+                    draggedImageView.setVisibility(View.VISIBLE);
+                }
+                return true;
+
+            // An unknown action type was received.
+            default:
+                Log.i(TAG, "Unknown action type received by OnDragListener.");
+                break;
+        }
+        return false;
+    }
 }
--- app/src/main/java/message/app/com/emo/adapter/RecyclerAdapter.java
+++ app/src/main/java/message/app/com/emo/adapter/RecyclerAdapter.java
@@ -1,6 +1,8 @@
 package message.app.com.emo.adapter;
 
+import android.content.ClipData;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,13 +44,27 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         return mItems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
 
         public ImageView img;
 
         public ViewHolder(View itemView) {
             super(itemView);
             img = (ImageView) itemView.findViewById(R.id.img_item_recycler_view);
+            itemView.setOnLongClickListener(this);
+        }
+
+
+
+        @Override
+        public boolean onLongClick(View imageView) {
+            Log.i("xxx", "12345");
+            ClipData clipData = ClipData.newPlainText("", "" + mItems.get(getPosition()).getResourceId());
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(imageView);
+            /*start the drag - contains the data to be dragged,
+            metadata for this data and callback for drawing shadow*/
+            imageView.startDrag(clipData, shadowBuilder, imageView, 0);
+            return true;
         }
     }
 }
